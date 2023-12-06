15ae00e81e4aa3a49790702860238038552f9afd
정빈 박 <tim0402@naver.com>
2023-12-06 16:40:54 +0900

    math 함수 추가

--- JBEngine_SOURCE/JBMath.h
+++ JBEngine_SOURCE/JBMath.h
@@ -2,6 +2,8 @@
 
 namespace JB::math
 {
+#define PI 3.141592f
+
 	struct Vector2
 	{
 		static Vector2 One;
@@ -44,6 +46,46 @@ namespace JB::math
 			return Vector2(x * value, y * value);
 		}
 
+		void clear()
+		{
+			x = 0.0f;
+			y = 0.0f;
+		}
+
+		float length()
+		{
+			return sqrtf(x * x + y * y);
+		}
+
+		Vector2 normalize()
+		{
+			float len = length();
+			x /= len;
+			y /= len;
+
+			return *this;
+		}
+
 	};
+
+	Vector2 Rotate(Vector2 vector, float degree)
+	{
+		float radian = (degree / 180.f) * PI;
+		vector.normalize();
+		float x = cosf(radian) * vector.x - sinf(radian) * vector.y;
+		float y = sinf(radian) * vector.x + cosf(radian) * vector.y;
+
+		return Vector2(x, y);
+	}
+
+	float Dot(Vector2& v1, Vector2 v2)
+	{
+		return v1.x * v2.x + v1.y * v2.y;
+	}
+
+	float Cross(Vector2 v1, Vector2 v2)
+	{
+		return v1.x * v2.y - v1.y * v2.x;
+	}
 }
 
